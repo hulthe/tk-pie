2b66c022f7d950b028ded8327d0413de6639c2ae
Joakim Hulthe <joakim@hulthe.net>
2023-05-08 19:43:18 +0200

    Fix right.rs

--- src/bin/right.rs
+++ src/bin/right.rs
@@ -29,7 +29,7 @@ async fn main(_spawner: Spawner) {
     let _neopixel_power = Output::new(board.neopixel_power, Level::High);
 
     let mut neopixel = Ws2812::new(board.PIO0, board.DMA_CH0, board.neopixel.degrade());
-    let mut neopixels_d5 = Ws2812::new(board.PIO1, board.DMA_CH1, board.d5.degrade());
+    let neopixels_d5 = Ws2812::new(board.PIO1, board.DMA_CH1, board.d5.degrade());
 
     neopixel.write(&[Rgb::new(0xFF, 0x00, 0x00)]).await;
     usb::setup_logger_and_keyboard(board.USB).await;
@@ -44,46 +44,40 @@ async fn main(_spawner: Spawner) {
     };
 
     let keyboard = KeyboardConfig {
-        layers,
         pins: [
             // TODO: reconfigure these for right PCB
             // row 1
-            board.d24.degrade(),
-            board.a3.degrade(),
-            board.a2.degrade(),
-            board.a1.degrade(),
-            board.a0.degrade(),
-            // row 2
-            board.d25.degrade(),
-            board.sck.degrade(),
-            board.mosi.degrade(),
-            board.miso.degrade(),
-            board.d2.degrade(),
-            // row 3
             board.d12.degrade(),
             board.d11.degrade(),
             board.d10.degrade(),
             board.d9.degrade(),
-            board.d3.degrade(),
-            // thumbpad
             board.d7.degrade(),
-            board.scl.degrade(),
+            // row 2
+            board.a2.degrade(),
+            board.a1.degrade(),
+            board.a0.degrade(),
             board.sda.degrade(),
+            board.scl.degrade(),
+            // row 3
+            board.mosi.degrade(),
+            board.sck.degrade(),
+            board.d25.degrade(),
+            board.d24.degrade(),
+            board.a3.degrade(),
+            // thumbpad
+            board.d3.degrade(),
+            board.d2.degrade(),
+            board.miso.degrade(),
         ],
+        led_map: [0, 1, 2, 3, 4, 9, 8, 7, 6, 5, 10, 11, 12, 13, 14, 15, 16, 17],
+        led_driver: neopixels_d5,
+        layers,
     };
 
     keyboard.create().await;
 
     for w in 0usize.. {
         neopixel.write(&[wheel(w as u8)]).await;
-        neopixels_d5
-            .write(&[
-                wheel((w + 50) as u8),
-                wheel((w + 100) as u8),
-                wheel((w + 150) as u8),
-                wheel((w + 200) as u8),
-            ])
-            .await;
         Timer::after(Duration::from_millis(10)).await;
     }
 }
